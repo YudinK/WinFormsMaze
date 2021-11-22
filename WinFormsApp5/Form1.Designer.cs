796af7a66db2f9737eadd80d57c2a925872f8620
BluskerI <87750237+BluskerI@users.noreply.github.com>
2021-11-22 22:31:09 +0200

    Добавьте файлы проекта.

---
+++ WinFormsApp5/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace WinFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Click";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.panel1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WinFormsApp5/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        int side = 20;
+        State[,] Map;
+        int width = 61;
+        int height = 31;
+        Point currentCell;
+        Stack<Point> stack;
+        Random rnd;
+        bool builded = false;
+        public enum State
+        {
+            Wall,
+            Cell,
+            Visited
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Size = new Size(width * side, height * side);
+            stack = new Stack<Point>();
+            rnd = new Random(); 
+            Map = new State[width, height];
+            InitMap();            
+            currentCell = new Point(1, 1);
+        }
+        private void InitMap()
+        {
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = 0; j < height; j++)
+                {
+                    if (i % 2 != 0 && j % 2 != 0 &&         
+                        i < width - 1 && j < height - 1)    
+                        Map[i, j] = State.Cell;             
+                    else Map[i, j] = State.Wall;            
+                }
+            }
+        }       
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                {
+                    switch (Map[x, y])
+                    {
+                        case State.Cell:
+                            DrawBox(x, y, Brushes.White, e.Graphics);
+                            break;
+                        case State.Wall:
+                            DrawBox(x, y, Brushes.Black, e.Graphics);
+                            break;
+                        case State.Visited:
+                            DrawBox(x, y, Brushes.Red, e.Graphics);
+                            break;
+                    }
+                }
+        }
+        void DrawBox(int x, int y, Brush br, Graphics gr)
+        {
+            gr.FillRectangle(br, x * side, y * side, side, side);
+        }
+        private void BuildMap()
+        {
+            while (true)
+            {
+                var neighbours = GetNeighbours(currentCell);
+                if (neighbours.Length != 0)
+                {
+                    var randNum = rnd.Next(neighbours.Length);
+                    var neighbourCell = neighbours[randNum];
+                    if (neighbours.Length > 1)
+                        stack.Push(currentCell); 
+                    RemoveWall(currentCell, neighbourCell);
+                    Map[currentCell.X, currentCell.Y] = State.Visited;
+                    currentCell = neighbourCell;
+                }
+                else if (stack.Count > 0) 
+                {
+                    Map[currentCell.X, currentCell.Y] = State.Visited;
+                    currentCell = stack.Pop();
+                }
+                else
+                    break;
+            }
+        }
+        private void PrepareAfterBuildMap()
+        {
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = 0; j < height; j++)
+                {
+                    if (Map[i, j] == State.Visited)
+                        Map[i, j] = State.Cell;
+                }
+            }
+        }
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            if (builded)
+            {
+                InitMap();
+                currentCell = new Point(1, 1);
+            }
+            BuildMap();
+            PrepareAfterBuildMap();
+            builded = true;
+            panel1.Invalidate();
+        }
+        private void RemoveWall(Point first, Point second)
+        {
+            var xDiff = second.X - first.X;
+            var yDiff = second.Y - first.Y;
+            int addX, addY;
+            var target = new Point();
+
+            addX = (xDiff != 0) ? (xDiff / Math.Abs(xDiff)) : 0;
+            addY = (yDiff != 0) ? (yDiff / Math.Abs(yDiff)) : 0;
+
+            target.X = first.X + addX; 
+            target.Y = first.Y + addY;
+
+            Map[target.X, target.Y] = State.Visited;
+        }
+        private Point[] GetNeighbours(Point c)
+        {
+            const int distance = 2;
+            var points = new List<Point>();
+            var x = c.X;
+            var y = c.Y;
+            var up = new Point(x, y - distance);
+            var rt = new Point(x + distance, y);
+            var dw = new Point(x, y + distance);
+            var lt = new Point(x - distance, y);
+            var d = new Point[] { dw, rt, up, lt };
+            foreach (var p in d)
+            {
+                if (p.X > 0 && p.X < width && p.Y > 0 && p.Y < height)
+                {
+                    if (Map[p.X, p.Y] != State.Wall && Map[p.X, p.Y] != State.Visited)
+                        points.Add(p);
+                }
+            }
+            return points.ToArray();
+        }
+    }
+}
